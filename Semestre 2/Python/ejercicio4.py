a06afa650465271fbc51a76daca893aef6319af2
Jose Luis Carrera Ballesteros <76192462+josecarbs08@users.noreply.github.com>
2026-02-25 20:50:57 +0100

    ejercicio4

---
+++ Semestre 2/Python/ejercicio4.py
@@ -0,0 +1,18 @@
+day = int(input('Ingrese un día (en minúsculas): '))
+
+if day == 'lunes':
+    print('Hoy es lunes, ¡comienza la semana!')
+elif day == 'martes':
+    print('Hoy es martes, ¡sigue adelante!')
+elif day == 'miércoles':
+    print('Hoy es miércoles, ¡ya casi es fin de semana!')
+elif day == 'jueves':
+    print('Hoy es jueves, ¡casi es viernes!')
+elif day == 'viernes':
+    print('Hoy es viernes, ¡por fin es fin de semana!')
+elif day == 'sábado':
+    print('Hoy es sábado, ¡disfruta tu día libre!')
+elif day == 'domingo':
+    print('Hoy es domingo, ¡descansa y prepárate para la semana!')
+else:
+    print('Día no válido. Por favor, ingrese un día de la semana en minúsculas.')
