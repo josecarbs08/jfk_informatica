7306edf5bf5a1b436719d683bf252d5d411a1382
Jose Luis Carrera Ballesteros <76192462+josecarbs08@users.noreply.github.com>
2026-04-21 10:14:03 -0600

    sd

---
+++ Semestre 2/Python/9.- ejercicio9.py
@@ -0,0 +1,17 @@
+for i in range (10):
+    print('La secuencia es:',i)
+
+for i in range (5, 20):
+    print('La secuencia es:',i)
+
+
+for i in range (10, 50, 2):
+    print('La secuencia es:',i)
+
+
+#ejercicio1
+
+max = int(input('Ingrese el numero maximo: '))
+
+for i in range (max, 0,-1):
+    print('La secuencia es:',i)
---
+++ Semestre 2/tortuga/tortuga1.py
@@ -0,0 +1,13 @@
+from turtle import *
+
+bgcolor("black")
+speed(0)
+color("yellow")
+circle(10)
+
+
+for i in range(20):
+    circle(10*1)
+    left(i)
+
+done()
